492c28a482b2757a441e3dbc986bf024969d1e78
Aakash <caakashct@gmail.com>
2025-12-03 18:09:10 +0530

    Day 2(Update blog web like make responsive and interactive)

---
+++ fisrt_week/task 2-simple_web_page/index.js
@@ -0,0 +1,18 @@
+function pop(){
+    window.alert("Contact soon")
+}
+
+
+// function addblog(){
+//     window.alert("The blog was added")
+// }
+
+function sum(){
+    const nameInput = document.querySelectorAll("input");
+
+    nameInput.addEventListener("input", () => {
+    nameInput.setCustomValidity(""
+    nameInput.addEventListener()("invalid"()=>{
+    })
+});
+}
