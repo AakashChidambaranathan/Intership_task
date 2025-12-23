0470b045f604410b0cb1eeecd44a8d810df89475
Aakash <caakashct@gmail.com>
2025-12-23 17:19:15 +0530

    json store a data from form

---
+++ third_week/nodejs code/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.send("this worink");
+});
+
+app.post("/write", (req, res) => {
+  const message = req.body.message;
+  fs.writeFile("data.text", message, (err) => {
+    if (err) {
+      return res.status(500).send("data is store");
+    }
+    res.send("data is store");
+  });
+});
+
+app.get("/read", (req, res) => {
+  fs.readFile("./text.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Cannot read file");
+    }
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
---
+++ third_week/nodejs code/promises.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const cors=require('cors')
+const fs=require('fs');
+const { json } = require('body-parser');
+const app=express();
+app.use(cors());
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("this home page")
+})
+app.post('/user',async(req,res)=>{
+    try{
+        const {name,email}=req.body;
+        if(!name||!email){
+            return res.status(400).send("this have error")
+        } 
+        const data=await fs.readFile("users.json","utf-8")
+        const users=JSON.parse(data)
+        const newUser={
+            name,email
+        }
+        users.push(newUser)
+        await fs.writeFile("users.json",JSON.stringify('users',null,2))
+        res.send("data is store")
+    }catch(err){
+        res.send("this have error")
+    }    
+})
+
+app.get('/users',(req,res)=>{
+    try{
+        const data=fs.readFile('user.json',"utf-8")
+        const used=JSON.parse(data)
+        res.json(used)
+    }catch(err){
+        res.send("this have the error")
+    }
+})
+app.listen(3000)
